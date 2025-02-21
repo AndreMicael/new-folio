3498da639eecf91094f29d09b683f2271a32829d
Andre <andremicael@gmail.com>
2025-02-21 10:11:37 -0400

    conectando ao strapi remoto

--- frontend/app/api/projetos/route.ts
+++ frontend/app/api/projetos/route.ts
@@ -1,25 +1,29 @@
 export const dynamic = "force-static";
 export const revalidate = 60; // Revalidate every 60 seconds
 
-import {  NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-
 
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    const response = await fetch('http://localhost:1337/api/projetos?populate=image');
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/projetos?populate=image`
+    );
     if (!response.ok) {
-      throw new Error('Erro ao buscar os projetos');
+      throw new Error("Erro ao buscar os projetos");
     }
 
     const data = await response.json();
 
     return NextResponse.json(data);
   } catch (error) {
-    return new NextResponse(JSON.stringify({ error: (error as Error).message }), {
-      status: 500,
-    });
+    return new NextResponse(
+      JSON.stringify({ error: (error as Error).message }),
+      {
+        status: 500,
+      }
+    );
   }
 }
--- frontend/app/projetos/[slug]/ProjectContent.tsx
+++ frontend/app/projetos/[slug]/ProjectContent.tsx
@@ -1,24 +1,23 @@
-'use client';
+"use client";
 import React, { useEffect, useState } from "react";
-import { fetchProjetos } from "@/app/projetos/[slug]/fetchProjetos"; 
+import { fetchProjetos } from "@/app/projetos/[slug]/fetchProjetos";
 import type Projeto from "@/interfaces/Projeto";
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface Props {
-    slug: string;
+  slug: string;
 }
 
 const ProjectContent: React.FC<Props> = ({ slug }) => {
-
   const [projects, setProjects] = useState<Projeto[]>([]);
 
   useEffect(() => {
     const loadProjects = async () => {
-      const fetchedProjects = await fetchProjetos(); 
-      setProjects(fetchedProjects); 
+      const fetchedProjects = await fetchProjetos();
+      setProjects(fetchedProjects);
     };
 
     loadProjects();
@@ -26,43 +25,46 @@ const ProjectContent: React.FC<Props> = ({ slug }) => {
 
   return (
     <div>
-     {projects.filter(project => project.slug === slug)
-  .map(project => (
-    <div key={project.id}>
-        <img src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${project.image.formats.medium.url}`} alt={project.title} />
-      <h2>{project.title}</h2>
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code({node, inline, className, children, ...props}) {
-            const match = /language-(\w+)/.exec(className || '')
-            return !inline && match ? (
-              <SyntaxHighlighter
-                style={materialDark}
-                language={match[1]}
-                PreTag="div"
-                {...props}
-              >
-                {String(children).replace(/\n$/, '')}
-              </SyntaxHighlighter>
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            )
-          }
-        }}
-      >
-        {project.description}
-      </ReactMarkdown>
-      <ul>
-        {project.stacks.map(stack => (
-          <li key={stack}>{stack}</li>
+      {projects
+        .filter((project) => project.slug === slug)
+        .map((project) => (
+          <div key={project.id}>
+            <img
+              src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${project.image.formats.medium.url}`}
+              alt={project.title}
+            />
+            <h2>{project.title}</h2>
+            <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || "");
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      style={materialDark}
+                      language={match[1]}
+                      PreTag="div"
+                      {...props}
+                    >
+                      {String(children).replace(/\n$/, "")}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            >
+              {project.description}
+            </ReactMarkdown>
+            <ul>
+              {project.stacks.map((stack) => (
+                <li key={stack}>{stack}</li>
+              ))}
+            </ul>
+          </div>
         ))}
-      </ul>
-    </div>
-))}
-
     </div>
   );
 };
